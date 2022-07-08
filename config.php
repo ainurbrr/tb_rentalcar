c739e25b20b1463dd4361a61f81bf730ee3cd4aa
Your Name <you@example.com>
2022-07-08 22:19:32 +0700

    commit all

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php 
+
+if(!defined('BASEPATH')) echo "Tidak bisa langsung mengakses halaman ini!";
+
+define('DEFAULT_CONTROLLER', 'C_Auth');
+define('BASE_URL', 'http://localhost/rentalcar/');
+define('APP_NAME', 'Rental Car Admin');
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'rentalcar');
+
+?>
---
+++ views/_partials/footer.php
@@ -0,0 +1,8 @@
+<footer class="sticky-footer bg-white">
+	<div class="container my-auto">
+	    <div class="copyright text-center my-auto">
+	    <span>
+	    <a href="https://github.com/ainurbrr" target="_blank">2022 Moh. Ainur Bahtiar Rohman</a> &copy; Admin Rental Car <?= date('Y') ?></span>
+	    </div>
+	</div>
+</footer>
